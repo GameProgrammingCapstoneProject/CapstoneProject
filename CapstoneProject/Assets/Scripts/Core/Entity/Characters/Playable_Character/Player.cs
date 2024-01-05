f8d90c39af77787bf225f3df516aab4c4799ae16
PhuocHoang <htphuoc1995@gmail.com>
2024-01-04 23:40:24 -0500

    Added new Character class inherited from Entity, and new Item class

--- CapstoneProject/Assets/Scripts/Core/Entity/Characters/Playable_Character/Player.cs
+++ CapstoneProject/Assets/Scripts/Core/Entity/Characters/Playable_Character/Player.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 
 namespace Core.Entity
 {
-    public class Player : Entity
+    public class Player : Character
     {
-        private float _jumpForce = 5f;
+        [SerializeField]
+        private float _jumpForce = 8f;
         private bool _isBusy = false;
         public EntityStateMachine<PlayerState> stateMachine { get; private set; }
         public PlayerIdleState idleState { get; private set; }
         public PlayerRunState runState { get; private set; }
         public PlayerAirState airState { get; private set; }
         public PlayerJumpState jumpState { get; private set; }
-        public PlayerGroundAttack groundAttackState { get; private set; }
+        public PlayerGroundAttackState groundAttackState { get; private set; }
+        public PlayerAirAttackState airAttackState { get; private set; }
+        public PlayerInteractState interactState { get; private set; }
         protected override void Awake()
         {
             base.Awake();
@@ -24,7 +27,9 @@ namespace Core.Entity
             runState = new PlayerRunState(this, "Run");
             airState = new PlayerAirState(this, "Jump");
             jumpState = new PlayerJumpState(this, "Jump");
-            groundAttackState = new PlayerGroundAttack(this, "GroundAttack");
+            groundAttackState = new PlayerGroundAttackState(this, "GroundAttack");
+            airAttackState = new PlayerAirAttackState(this, "AirAttack");
+            interactState = new PlayerInteractState(this, "Interact");
         }
         protected override void Start()
         {
@@ -32,8 +37,9 @@ namespace Core.Entity
             stateMachine.Initialize(idleState);
         }
         
-        void Update()
+        protected override void Update()
         {
+            base.Update();
             stateMachine.currentState.StateUpdate();
         }
         
@@ -48,7 +54,7 @@ namespace Core.Entity
         protected override void InitialSetup()
         {
             base.InitialSetup();
-            SetJumpForce(8f);
+            SetJumpForce(_jumpForce);
         }
 
         public float GetJumpForce() => _jumpForce;
--- CapstoneProject/Assets/Scripts/Core/Entity/Entity.cs
+++ CapstoneProject/Assets/Scripts/Core/Entity/Entity.cs
@@ -8,22 +8,14 @@ namespace Core.Entity
     /// <summary>
     ///     A basic entity type which designed to be inherited by other entities.
     /// </summary>
-    public class Entity : MonoBehaviour
+    public abstract class Entity : MonoBehaviour
     {
-        public enum FacingDirections
-        {
-            LEFT,
-            RIGHT
-        }
-
-        private float _moveSpeed = 2f;
-        
-        // The direction that the entity is facing
-        public FacingDirections CurrentFacingDirection { get; private set; } = FacingDirections.RIGHT;
-        public Rigidbody2D rb { get; private set; }
-        public Animator animator { get; private set; }
+        public SpriteRenderer spriteRenderer { get; private set; }
 
         protected virtual void Awake()
+        {
+        }
+        protected virtual void OnEnable()
         {
         }
 
@@ -31,47 +23,9 @@ namespace Core.Entity
         {
             InitialSetup();
         }
-
-        /// <summary>
-        ///     Flip the sprite to the opposite direction.
-        /// </summary>
-        public virtual void Flip()
-        {
-            CurrentFacingDirection = (CurrentFacingDirection == FacingDirections.RIGHT) ? 
-                FacingDirections.LEFT : FacingDirections.RIGHT;
-            transform.Rotate(0, 180, 0);
-        }
         
-        /// <summary>
-        ///     Control if the sprite should be flipped based on the Horizontal Velocity
-        /// </summary>
-        public void ControlFlip(float horizontalVelocity)
+        protected virtual void Update()
         {
-            if (horizontalVelocity > 0 && CurrentFacingDirection == FacingDirections.LEFT)
-            {
-                Flip();
-            }
-            else if (horizontalVelocity < 0 && CurrentFacingDirection == FacingDirections.RIGHT)
-            {
-                Flip();
-            }
-        }
-        
-        /// <summary>
-        ///     Reset the entity's velocity to 0
-        /// </summary>
-        public void ResetToZeroVelocity()
-        {
-            rb.velocity = Vector2.zero;
-        }
-        
-        /// <summary>
-        ///     Set the entity's velocity
-        /// </summary>
-        public void SetVelocity(float horizontalVelocity, float verticalVelocity)
-        {
-            rb.velocity = new Vector2(horizontalVelocity, verticalVelocity);
-            ControlFlip(horizontalVelocity);
         }
 
         /// <summary>
@@ -79,13 +33,8 @@ namespace Core.Entity
         /// </summary>
         protected virtual void InitialSetup()
         {
-            rb = GetComponent<Rigidbody2D>();
-            animator = GetComponentInChildren<Animator>();
-            SetMoveSpeed(6f);
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         }
-
-        public float GetMoveSpeed() => _moveSpeed;
-        public float SetMoveSpeed(float moveSpeed) => _moveSpeed = moveSpeed;
     }
 }
 
