d01752475967d8063ed69b09445ca2911b7108ef
Muhammad Ashhad <Betadrone596@gmail.com>
2024-01-30 23:31:05 -0500

    Created Necessary files for BT Viewer
    BehaviorTreeView, InspectorView, SplitView, NodeView, BehaviorTreeEditor all added for viewer. Composite, Decorator, root, and BehaviorTree scripts modified/added.

---
+++ CapstoneProject/Assets/Scripts/Core/BehaviorTree/Editor/InspectorView.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InspectorView : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<InspectorView, VisualElement.UxmlTraits> { }
+    
+    Editor editor;
+    public InspectorView()
+    {
+
+    }
+
+    internal void UpdateSelection(NodeView nodeView)
+    {
+        Clear();
+
+        UnityEngine.Object.DestroyImmediate(editor);
+        editor = Editor.CreateEditor(nodeView.node);
+        IMGUIContainer container = new IMGUIContainer(() => { editor.OnInspectorGUI(); });
+        Add(container);
+    }
+}
