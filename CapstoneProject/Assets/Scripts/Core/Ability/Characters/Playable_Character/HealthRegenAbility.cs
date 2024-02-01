952f490ba15bb58b12536389933ac494a4f0856a
PhuocHoang <htphuoc1995@gmail.com>
2024-02-01 10:33:45 -0500

    Added health regen ability

---
+++ CapstoneProject/Assets/Scripts/Core/Ability/Characters/Playable_Character/HealthRegenAbility.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities/Health Regen Ability", fileName = "HealthRegenAbility", order = 3)]
+public class HealthRegenAbility : PlayerAbility
+{
+    [SerializeField]
+    private HealthRegen _healthRegenPrefab;
+    private HealthRegen _currenthealthRegen;
+    private readonly float _yOffset = 1f;
+    public event System.Action OnHealthRegenAbilityCoolDown;
+    protected override void Activate()
+    {
+        if (_currenthealthRegen == null)
+        {
+            _currenthealthRegen = GameObject.Instantiate(_healthRegenPrefab, new Vector3(Instigator.transform.position.x, Instigator.transform.position.y + _yOffset, Instigator.transform.position.z), Instigator.transform.rotation);
+            _currenthealthRegen.Setup(Instigator);
+            OnHealthRegenAbilityCoolDown?.Invoke();
+            Debug.Log("Health Regen Activated");
+        }
+    }
+}
