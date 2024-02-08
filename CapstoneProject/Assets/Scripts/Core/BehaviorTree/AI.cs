21b5c85d535c3222baca93cc208606155a4cb9b2
Muhammad Ashhad <Betadrone596@gmail.com>
2024-02-08 09:36:09 -0500

    Made roam behavior
    Used pandaBT to make the melee enemy roam and not fall of the platform

--- CapstoneProject/Assets/Scripts/Core/BehaviorTree/AI.cs
+++ CapstoneProject/Assets/Scripts/Core/BehaviorTree/AI.cs
@@ -10,12 +10,22 @@ public class AI : MonoBehaviour
     Enemy enemy;
     RigidbodyComponent _rb;
     Animator _anim;
+    float movementSpeed = 2.0f;
+
+    [SerializeField]
+    Transform CastPos;
+    [SerializeField]
+    float baseCastDistance;
+
+    RigidbodyComponent.FacingDirections currentFacingDirection;
+
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponent<Enemy>();
-        _anim = GetComponent<Animator>();
-        _rb = GetComponent<RigidbodyComponent>();
+        _anim = enemy.animator;
+        _rb = enemy.rb;
+        currentFacingDirection = _rb.CurrentFacingDirection;
     }
 
     // Update is called once per frame
@@ -24,10 +34,54 @@ public class AI : MonoBehaviour
         
     }
 
+    bool IsHittingWallOrEdge()
+    {
+        bool result;
+
+        currentFacingDirection = _rb.CurrentFacingDirection;
+        // Define the cast distance for left and right
+        float castingDistance = baseCastDistance;
+        if (currentFacingDirection == RigidbodyComponent.FacingDirections.LEFT )
+        {
+            castingDistance = -baseCastDistance;
+        }
+
+        // determine the target destination based on the cast distance
+        Vector3 targetPos = CastPos.position;
+        targetPos.x += castingDistance;
+
+        Debug.DrawLine(CastPos.position, targetPos, Color.red);
+        Debug.DrawRay(CastPos.position, Vector2.down, Color.red);
+        if (Physics2D.Linecast(CastPos.position, targetPos, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+
+        if (Physics2D.Raycast(CastPos.position, -Vector2.up, baseCastDistance, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            result = false;
+        }
+        else
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
     [Task]
     public void Roam()
     {
-        _rb.SetVelocity( -1.0f, _rb.velocity.y);
+        movementSpeed = (IsHittingWallOrEdge() ? -movementSpeed : movementSpeed);
+        if(movementSpeed > 0.1f || movementSpeed < -0.1f)
+        {
+            _anim.SetInteger("state", 1);
+        }
+        _rb.SetVelocity(movementSpeed, _rb.velocity.y);
         Task.current.Succeed();
     }
 }
