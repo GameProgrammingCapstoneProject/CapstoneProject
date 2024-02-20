caebbc5ed9116a8fa545bab3e5c1153bda47c0f6
PhuocHoang <htphuoc1995@gmail.com>
2024-02-19 21:45:00 -0500

    Updated Damageable Interface

--- CapstoneProject/Assets/Scripts/Core/GameplayInterface/IDamageable.cs
+++ CapstoneProject/Assets/Scripts/Core/GameplayInterface/IDamageable.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Core.Entity;
 using UnityEngine;
 
 public interface IDamageable
 {
     public void TakeDamage(int damage);
-    public void DoDamage(int damage);
+    public void DoDamage(int damage, Character target);
 }
