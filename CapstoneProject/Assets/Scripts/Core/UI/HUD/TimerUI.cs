0a1cd8b066037833d445088759e7df54f3407b89
Joshua Muller <Joshua12450@gmail.com>
2024-03-07 11:49:34 -0500

    added very basic timer
    just counts up. add the prefab to the canvas to test

--- CapstoneProject/Assets/Scripts/Core/UI/HUD/TimerUI.cs
+++ CapstoneProject/Assets/Scripts/Core/UI/HUD/TimerUI.cs
@@ -17,6 +17,18 @@ public class TimerUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeIsRunning && timeRemaining >= 0)
+        {
+            timeRemaining += Time.deltaTime;
+            DisplayTime(timeRemaining);
+        }
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
     }
 }
