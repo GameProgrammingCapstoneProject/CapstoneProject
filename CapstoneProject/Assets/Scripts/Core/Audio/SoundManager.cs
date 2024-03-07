34ca37bebd09ba5d166f894fa69f0373b4a90822
Joshua Muller <Joshua12450@gmail.com>
2024-03-07 10:54:48 -0500

    added function to adjust music volume seperately
    made it so there's a playmusic and a playloopedmusic function that uses a different mod, therefore allowing it to be adjusted seperately from sfx

--- CapstoneProject/Assets/Scripts/Core/Audio/SoundManager.cs
+++ CapstoneProject/Assets/Scripts/Core/Audio/SoundManager.cs
@@ -7,16 +7,26 @@ using System;
 /* GUIDE TO USING SOUND MANAGER
    BY JOSHUA MULLER
 
-To use the manager, firstly find the SoundManager prefab object and place it into the scene.
-There should be two example sounds attached, Player Jump and Player Death.
-If not, add these sounds in if needed.
+To use the manager, firstly find the SoundManager prefab object and place it into the scene, if it isn't already placed.
 Then, add any sound you need. This should be simple enough, just hit the + At the bottom and add a sound.
-You'll need a name, and a clip. Just drag a clip in and give it an apporiate name. Set Volume and Pitch to 1, or fiddle with the values if you please.
+You'll need a name, and a clip. Just drag a clip in and give it an apporiate name. Set Volume and Pitch to 1, or fiddle with the values if you please. 
+Volume should stay at 1 unless absolutely necessary, but it's good to fiddle with pitch to make sounds sound different or better.
 Then, to call the sound, the simplest way is to just add the following line into code that runs whenever you want the sound to happen: 
 Object.FindObjectOfType<SoundManager> ().Play("Sound Name");
 Of course, replace Sound Name with the name of your sound AS IT APPEARS IN THE NAME SECTION OF THE SOUND MANAGER SCRIPT, NOT THE ACUTAL SOUND'S NAME 
 Although, these should be the same in most cases anyway.
 An example is in PlayerJumpState, causing the PlayerJump sound to play whenever the player's jump state starts.
+if you want to play a looped sound NOT MUSIC, use
+Object.FindObjectOfType<SoundManager> ().PlayLooped("Sound Name");
+if you want to stop a sound from playing instantly, use
+Object.FindObjectOfType<SoundManager> ().Stop("Sound Name");
+if you want to stop a sound/music from looping but let it finish playing one last time, use
+Object.FindObjectOfType<SoundManager> ().StopLooping("Sound Name");
+Finally, if you want to play music that doesn't loop, (important to make sure adjusting music volumes in settings), use
+Object.FindObjectOfType<SoundManager> ().PlayMusic("Sound Name");
+if you want to play music that loops, use
+Object.FindObjectOfType<SoundManager> ().PlayLoopedMusic("Sound Name");
+
 
 If you need any more help, please contact Joshua.
 */
@@ -24,16 +34,20 @@ public class SoundManager : MonoBehaviour{
 
     public Sound[] sounds;
     public float soundMod;
+    public float musicMod;
     private void Awake()
     {
         if (soundMod < 0 || soundMod > 1)
             soundMod = 1;
+
+        if (musicMod < 0 || musicMod > 1)
+            musicMod = 1;
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
 
-            s.source.volume = (s.volume * soundMod);
+            s.source.volume = s.volume;
             s.source.pitch = s.pitch;
         }
        
@@ -67,6 +81,22 @@ public class SoundManager : MonoBehaviour{
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.loop = false;
      
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.volume = (s.volume * musicMod);
+        s.source.loop = false;
+
+    }
+
+    public void PlayLoopedMusic(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.volume = (s.volume * musicMod);
+        s.source.loop = false;
+
     }
 
 }
