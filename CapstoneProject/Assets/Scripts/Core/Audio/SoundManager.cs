394e29e8d4f3f7dc02110447dfe9b4a73bec7f07
Joshua Muller <Joshua12450@gmail.com>
2024-03-12 12:14:41 -0400

    added more functions to sound manager again
    i must've accidentally deleted them in the merge conflict, but improved functionality is now back

--- CapstoneProject/Assets/Scripts/Core/Audio/SoundManager.cs
+++ CapstoneProject/Assets/Scripts/Core/Audio/SoundManager.cs
@@ -23,6 +23,9 @@ If you need any more help, please contact Joshua.
 public class SoundManager : MonoBehaviour{
 
     public Sound[] sounds;
+    public float soundMod = 1f;
+    public float musicMod = 1f;
+
     private void Awake()
     {
         foreach (Sound s in sounds)
@@ -38,6 +41,7 @@ public class SoundManager : MonoBehaviour{
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.volume = s.volume * soundMod;
         s.source.Play();
     }
 
@@ -45,7 +49,47 @@ public class SoundManager : MonoBehaviour{
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.loop = true;
+        s.source.volume = s.volume * soundMod;
         s.source.Play();
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.loop = true;
+        s.source.volume = s.volume * musicMod;
+        s.source.Play();
+    }
+
+    public void PlayMusicLooped(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.loop = true;
+        s.source.volume = s.volume * musicMod;
+        s.source.Play();
+    }
+
+    public void StopLooping(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.loop = false;
+
+    }
+
+    public void StopPlaying(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }
+
+    public void ChangeSoundVolume(float volume)
+    {
+        soundMod = volume;
+    }
+    
+    public void ChangeMusicVolume(float volume)
+    {
+        musicMod= volume;
     }
 
 }
